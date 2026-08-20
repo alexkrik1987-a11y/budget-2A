ad3cc0aa2b78140a8888785dd8abb1c42e8ac009
alexkrik1987-a11y <alexkrik1987@gmail.com>
2026-08-20 12:00:59 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const CACHE_NAME = "budget-2a-shell-v46";
+const CACHE_NAME = "budget-2a-shell-v47";
 const INDEX_URL = new URL("./index.html", self.location.href).href;
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=27",
+  "./styles.css?v=28",
   "./app.js?v=46",
   "./vendor/supabase.min.js?v=10",
   "./manifest.webmanifest",
